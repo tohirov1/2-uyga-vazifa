3adce94c4f834c4ef946cf3cbf19312b22e1dc67
Your Name <you@example.com>
2023-01-10 15:54:09 +0500

    git add

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var a = Math.round(Math.random()* 100)
+var b = Math.round(Math.random()* 100)
+
+var j = a-5;
+var d = b-5;
+
+if(j > d){
+    console.log(b + " 5 ga  yaqin" );
+} 
+else if(j < d){
+    console.log( a + " 5 ga yaqin");
+}
+else if( j === d){
+      console.log( j + " va" + d + " 5 ga ikalasi ham yaqin");
+}
+   
+
+
+
+console.log(a,b);
+
